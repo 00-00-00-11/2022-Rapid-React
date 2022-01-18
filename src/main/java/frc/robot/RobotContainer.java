ced9264f4b64d0982bf31cada16bc841f8cbafb5
etong11 <53305901+etong11@users.noreply.github.com>
2022-01-18 15:20:19 -0500

    2021 auto code
    Copied from auto code from 2021 repo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,11 +2,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import java.io.IOException;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.Filesystem;
+import java.nio.file.Path;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
+
 public class RobotContainer {
 
   // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  public final static DriveSubsystem m_driveSubsystem = new DriveSubsystem();
 
   public RobotContainer() {
 
@@ -16,7 +26,38 @@ public class RobotContainer {
   private void configureButtonBindings() {}
 
   public Command getAutonomousCommand() {
-    return null;
-    // return m_autoCommand;
+    TrajectoryConfig config = new TrajectoryConfig(
+      Units.feetToMeters(2), 
+      Units.feetToMeters(2)
+    );
+
+    config.setKinematics(m_driveSubsystem.getKinematics());
+
+ Trajectory pathWeaverTrajectory = new Trajectory();
+    Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("output/slalom.wpilib.json");
+    try {
+      pathWeaverTrajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    RamseteCommand command = new RamseteCommand(
+        pathWeaverTrajectory,
+        m_driveSubsystem::getPose,
+        new RamseteController(2.0, 0.7),
+        m_driveSubsystem.getFeedforward(),
+        m_driveSubsystem.getKinematics(),
+        m_driveSubsystem::getSpeeds,
+        m_driveSubsystem.getLeftPIDController(),
+        m_driveSubsystem.getRightPIDController(),
+        m_driveSubsystem::setOutput,
+        m_driveSubsystem 
+    );
+
+    // Reset odometry to the starting pose of the trajectory.
+    m_driveSubsystem.resetOdometry(pathWeaverTrajectory.getInitialPose());
+
+    // Run path following command, then stop at the end.
+    return command.andThen(() -> m_driveSubsystem.setOutput(0, 0));
   }
 }
