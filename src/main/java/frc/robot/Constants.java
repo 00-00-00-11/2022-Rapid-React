f7cad2332ca1b853020e9b1318109fdaa84497f0
SachanPatel <87036965+SachanPatel@users.noreply.github.com>
2022-01-18 21:12:56 -0500

    Added shooter command and subsystem.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,9 @@
 package frc.robot;
 
 public final class Constants {
-
+  public final class ShooterConstants {
+    int FEEDER_MOTOR_PORT = 0;
+    int FLY_WHEEL_MOTOR_PORT = 1;
+  }
   public final class DriveConstants {}
 }
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ShooterSubsystem extends SubsystemBase {
+    /* Shooter CANSpark Definition */ 
+    CANSparkMax feederMotor = new CANSparkMax(Constants.ShooterConstants.FEEDER_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax flyWheelMotor = new CANSparkMax(Constants.ShooterConstants.FLY_WHEEL_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    public ShooterSubsystem() {
+    }
+    /* Shoots the ball */
+    public void shootBalls() {
+        feederMotor.set(.5);
+        flyWheelMotor.set(.5);
+    }
+
+    @Override
+    public void periodic() {
+    // This method will be called once per scheduler run
+    }
+
+    @Override
+    public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+    }
+}
