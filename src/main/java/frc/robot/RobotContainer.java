4779921438576336e32d777c8bf8a800c06d4a2f
Siddharth Lohani <me@siddharthlohani.dev>
2022-01-28 13:09:18 -0500

    fixed format issues

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
@@ -21,7 +20,6 @@ public class RobotContainer {
     //   new POVButton(operatorGamepad, i).whileHeld(new QuickTurn(i));
     // }
 
-    
   }
 
   public Command getAutonomousCommand() {
