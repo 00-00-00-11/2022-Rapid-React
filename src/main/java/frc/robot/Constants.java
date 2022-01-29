f5ea8ff23b3e6d5e6cd67cfe3a8250e63c5cb9f2
Siddharth Lohani <me@siddharthlohani.dev>
2022-01-28 22:17:28 -0500

    added utility class and reformatted

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,11 @@
 package frc.robot;
 
 public final class Constants {
-    
-  public static class DriveConstants {
-    public static int INDEX_PORT = 10;   
+
+  public static class DriveConstants {}
+
+  public static class IndexerConstants {
+    public static final int INDEXER_MOTOR_CAN = 7;
+    public static final double INDEXER_SPEED = 1;
   }
 }
-
-
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,60 +1,25 @@
 package frc.robot;
 
-
-
-
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.PS4Controller;
-import edu.wpi.first.wpilibj.Joystick;
-//  import frc.robot.commands.ExampleCommand;
-//  import frc.robot.subsystems.ExampleSubsystem;
-//  import edu.wpi.first.wpilibj2.command.Command;  
-
-
-
-
-
-
+import frc.robot.commands.RunIndexer;
+import frc.robot.subsystems.IndexerSubsystem;
 
 public class RobotContainer {
-  public static  IndexerSubsystem m_IndexerSubsystem = new IndexerSubsystem();  
-  public static PS4Controller controller = new PS4Controller(0); 
-   //public static Joystick controller = new Joystick(); 
-
-  
-    public static JoystickButton triangleButton = new JoystickButton(controller, 4);
-
-
-  
-
-
-
-
-  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  // private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  public static IndexerSubsystem m_IndexerSubsystem = new IndexerSubsystem();
+  public static PS4Controller operatorGamepad = new PS4Controller(0);
+  public static JoystickButton indexerButton = new JoystickButton(operatorGamepad, 4);
 
   public RobotContainer() {
-    
-    
-    
-    
-    triangleButton.whileHeld(new IndexerCommand()); 
-           //  XboxController controller = new XboxController()  
-    
-    
-    
-
     configureButtonBindings();
   }
 
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    indexerButton.whileHeld(new RunIndexer());
+  }
 
   public Command getAutonomousCommand() {
     return null;
-    // return m_autoCommand;
   }
 }
