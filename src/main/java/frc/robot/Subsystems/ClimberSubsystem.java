bf49de5b3e958acbc1a29c12a21984ac0e61ec3a
redt1979 <advik360@gmail.com>
2022-02-05 11:47:40 -0500

    wat

--- src/main/java/frc/robot/Subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/Subsystems/ClimberSubsystem.java
@@ -22,7 +22,7 @@ Automated (Run Once Per Button):
 10. secondary returns to 0 angle
 */
 
-package frc.robot.subsystems;
+package frc.robot.Subsystems;
 
 import com.revrobotics.*;
 import edu.wpi.first.networktables.*;
@@ -34,8 +34,12 @@ import frc.robot.Constants;
 public class ClimberSubsystem extends SubsystemBase {
   /** Creates a new ClimberSubsystem. */
   CANSparkMax primaryElevatorMotor;
+  CANSparkMax primaryElevatorMotor2;
+  CANSparkMax primaryElevatorMotor3;
 
   CANSparkMax secondaryAnglerMotor;
+  CANSparkMax secondaryAnglerMotor2;
+  CANSparkMax secondaryAnglerMotor3;
 
   DigitalInput elevatorMaxSwitch;
   DigitalInput elevatorMinSwitch;
