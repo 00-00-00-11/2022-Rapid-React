0692bf2c239919d2cf15621635c47d5aac377c89
NoxiousFumes <55012494+NoxiousFumes@users.noreply.github.com>
2022-02-08 08:15:29 -0500

    sid formatter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -92,6 +92,7 @@ public class RobotContainer {
 
   private void configureButtonBindings() {
     autoElevatorButton.toggleWhenPressed(new AutoClimber(m_climberSubsystem));
+    autoElevatorBackButton.whenPressed(new AutoClimberBack(m_climberSubsystem));
 
     for (int i = 0; i < 360; i += 45) {
       new POVButton(driverController, i).whileHeld(new QuickTurn(i));
