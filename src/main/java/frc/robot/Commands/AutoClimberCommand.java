89e86c6d879d40631fa3d591384d9f9bcff3261e
redt1979 <advik360@gmail.com>
2022-02-12 18:47:29 -0500

    optimization

--- src/main/java/frc/robot/Commands/AutoClimberCommand.java
+++ src/main/java/frc/robot/Commands/AutoClimberCommand.java
@@ -35,7 +35,7 @@ public class AutoClimberCommand extends CommandBase {
 
   // Returns true when the command should end.
   @Override
-  public boolean isFinished() {
+  public boolean isFinished() { //can optimize
     if (RobotContainer.m_climberSubsystem.checkNextRungComplete()) {
       return true;
     } else {
