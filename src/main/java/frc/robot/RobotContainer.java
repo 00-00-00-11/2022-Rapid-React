79a8294d692621388524a96f0244ea71c2207794
Siddharth Lohani <me@siddharthlohani.dev>
2022-02-26 15:39:28 -0500

    removed unused imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,15 +1,11 @@
 package frc.robot;
 
-import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
-import frc.robot.utility.RamseteUtility;
-import frc.robot.utility.TrajectoryUtility;
 public class RobotContainer {
 
   public static final PS4Controller operatorGamepad = new PS4Controller(0);
--- src/main/java/frc/robot/utility/RamseteUtility.java
+++ src/main/java/frc/robot/utility/RamseteUtility.java
@@ -3,16 +3,11 @@ package frc.robot.utility;
 import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class RamseteUtility {
 
     public static RamseteCommand createRamseteCommand(Trajectory trajectory, DriveSubsystem driveSubsystem, boolean setPose) {
-        // if(setPose) {
-        //     RobotContainer.m_driveSubsystem.resetOdometry(trajectory.getInitialPose());
-        // } 
-
         return new RamseteCommand (
             trajectory,
             driveSubsystem::getPose,
