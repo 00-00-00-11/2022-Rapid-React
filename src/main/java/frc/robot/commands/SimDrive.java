9a7d3c78392f782d60b0fcd151ac7b74126e82c9
Siddharth Lohani <me@siddharthlohani.dev>
2022-02-28 12:43:02 -0500

    removed unused imports

--- src/main/java/frc/robot/commands/SimDrive.java
+++ src/main/java/frc/robot/commands/SimDrive.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.DriveSubsystem;
 
 public class SimDrive extends CommandBase {
 
