de2052fc286c06ad1c8368815b5646149c2d6277
Siddharth Lohani <me@siddharthlohani.dev>
2022-02-28 10:10:17 -0500

    chain indexer and shooter

--- src/main/java/frc/robot/commands/IndexerAndShoot.java
+++ src/main/java/frc/robot/commands/IndexerAndShoot.java
@@ -9,12 +9,12 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class IntakeAndIndex extends ParallelCommandGroup {
-  /** Creates a new IntakeAndIndex. */
-  public IntakeAndIndex() {
+public class IndexerAndShoot extends ParallelCommandGroup {
+  /** Creates a new IndexerAndShoot. */
+  public IndexerAndShoot() {
     addCommands(
-      new IntakeSpin(),
-      new RunIndexer()
+      new RunIndexer(),
+      new ShootBall()
     );
   }
 }
